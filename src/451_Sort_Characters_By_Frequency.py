fd4ceb17f8a70f9ca0cfc5dc29d8bf828e651235
BolgarMaxym97 <bolgar.maxym@gmail.com>
2024-02-07 12:49:18 +0200

    451 refactoring

--- src/451_Sort_Characters_By_Frequency.py
+++ src/451_Sort_Characters_By_Frequency.py
@@ -2,12 +2,12 @@ class Solution:
     def frequencySort(self, s: str) -> str:
         char_frequency = {}
         for char in s:
-            char_frequency[char] = char_frequency[char] + 1 if char in char_frequency else 1
-        char_frequency = dict(reversed(sorted(char_frequency.items(), key=lambda item: item[1])))
+            char_frequency[char] = char_frequency.get(char, 0) + 1
+        char_frequency = sorted(char_frequency.items(), key=lambda item: item[1], reverse=True)
         result = ""
 
         for char in char_frequency:
-            result += char * char_frequency[char]
+            result += char[0] * char[1]
         return result
 
 print(Solution.frequencySort(Solution, "tree"))
