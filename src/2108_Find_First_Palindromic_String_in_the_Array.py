62d253c7c959deb67bf7a75a4302dbd77773bc66
BolgarMaxym97 <bolgar.maxym@gmail.com>
2024-02-13 16:09:54 +0200

    2108

---
+++ src/2108_Find_First_Palindromic_String_in_the_Array.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for word in words:
+            if word == word[::-1]:
+                return word
+        return ""
+
+print(Solution.firstPalindrome(Solution, ["notapalindrome","racecar"]))
