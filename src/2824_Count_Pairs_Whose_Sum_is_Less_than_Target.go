8fa27230aac657adc94a1b3157a18ea7ef8d4ea9
BolgarMaxym97 <bolgar.maxym@gmail.com>
2025-01-20 11:14:46 +0200

    2824

---
+++ src/2824_Count_Pairs_Whose_Sum_is_Less_than_Target.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// TODO
+func countPairs(nums []int, target int) int {
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
+
+	l, r := 0, len(nums)-1
+	count := 0
+
+	for l < len(nums) {
+		if nums[r]+nums[l] < target {
+			count += r - l
+			l++
+		} else {
+			r--
+		}
+		if l == r {
+			break
+		}
+	}
+	return count
+}
+
+func main() {
+	fmt.Println(countPairs([]int{-6, 2, 5, -2, -7, -1, 3}, -2))
+
+}
